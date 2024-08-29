d170d9fceaebf47b4e41627f703a9d5c48725ddc
Vrush2004 <vrushalichaube@gmail.com>
2024-08-29 16:44:41 +0530

    order.js file added

---
+++ order.js
@@ -0,0 +1,25 @@
+var productPrices = {};
+
+$(function () {
+    //Json data by api call for order table
+    $.get(productListApiUrl, function (response) {
+        productPrices = {}
+        if(response) {
+            var options = '<option value="">--Select--</option>';
+            $.each(response, function(index, product) {
+                options += '<option value="'+ product.product_id +'">'+ product.name +'</option>';
+                productPrices[product.product_id] = product.price_per_unit;
+            });
+            $(".product-box").find("select").empty().html(options);
+        }
+    });
+});
+
+$("#addMoreButton").click(function () {
+    var row = $(".product-box").html();
+    $(".product-box-extra").append(row);
+    $(".product-box-extra .remove-row").last().removeClass('hideit');
+    $(".product-box-extra .product-price").last().text('0.0');
+    $(".product-box-extra .product-qty").last().val('1');
+    $(".product-box-extra .product-total").last().text('0.0');
+});
