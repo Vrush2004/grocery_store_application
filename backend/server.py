130208c70639c3bc1ea7b07d428b41f0cf2c35ec
Vrush2004 <vrushalichaube@gmail.com>
2024-09-06 12:13:16 +0530

    functionality added

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,10 @@
+import json
+
 from flask import Flask, request, jsonify
 
 from sql_connection import get_sql_connection
 import products_dao
+import uom_dao
 
 connection = get_sql_connection()
 
@@ -11,6 +14,23 @@ app = Flask(__name__)
 def hello():
     products = products_dao.get_all_products(connection)
     response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    response = uom_dao.get_uoms(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/insertProduct', methods=['POST'])
+def insert_product():
+    request_payload = json.loads(request.form['data'])
+    product_id = products_dao.insert_new_product(connection, request_payload)
+    response = jsonify({
+        'product_id':product_id
+    })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
